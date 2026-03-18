7381c522b108557ef116ee1d770bbfb11bc07b6c
Szymon Boćkowski <sbockowskix@gmail.com>
2026-03-18 09:00:40 +0100

    Add BudgetList and BudgetDetail views

--- apps/budgets/urls.py
+++ apps/budgets/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from apps.budgets.views import BudgetListView
+from apps.budgets.views import BudgetListView, BudgetDetailView
 
 urlpatterns = [
     path('api/budgets/', BudgetListView.as_view(), name='budget_list'),
+    path('api/budgets/<pk>', BudgetDetailView.as_view(), name='budget_detail'),
 ]
--- apps/budgets/views.py
+++ apps/budgets/views.py
@@ -1,7 +1,7 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
-
 from .models import Budget
 from .serializers import BudgetSerializer
 
@@ -21,4 +21,32 @@ class BudgetListView(APIView):
             budget = serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BudgetDetailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, request, pk):
+        try:
+            return Budget.objects.get(id=pk, user=request.user)
+        except Budget.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        budget = self.get_object(request, pk)
+        serializer = BudgetSerializer(budget)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        budget = self.get_object(request, pk)
+        serializer = BudgetSerializer(budget, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        budget = self.get_object(request, pk)
+        budget.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+

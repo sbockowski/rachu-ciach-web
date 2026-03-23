f31d3a3885149debacf138768063b4d3ae304357
Szymon Boćkowski <sbockowskix@gmail.com>
2026-03-23 19:30:55 +0100

    Set source to 'api' in TransactionListView

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -18,7 +18,7 @@ class TransactionListView(APIView):
     def post(self, request):
         serializer = TransactionSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save(user=request.user, source='api')
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

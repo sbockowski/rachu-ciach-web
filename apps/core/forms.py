d2a6783c11606e240d25f989d2314ff16ae735d5
Szymon Boćkowski <sbockowskix@gmail.com>
2026-03-25 10:05:18 +0100

    Refine register page

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -5,6 +5,17 @@ from django.contrib.auth.forms import UserCreationForm
 User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "input input-bordered w-full",
+            "placeholder": "Enter your username"
+        })
+    )
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
             "class": "input input-bordered w-full",
@@ -24,6 +35,10 @@ class UserRegisterForm(UserCreationForm):
         })
     )
 
-    class Meta:
-        model = User
-        fields = ['email', 'password1', 'password2']
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return cleaned_data

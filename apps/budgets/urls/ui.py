4931ca220fe5784bad89d5ec3d68e55691e6346f
Szymon Boćkowski <sbockowskix@gmail.com>
2026-03-25 16:32:42 +0100

    Add budget detail view and template

--- apps/budgets/urls/ui.py
+++ apps/budgets/urls/ui.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.budgets.views.ui import BudgetCreateView
+from apps.budgets.views.ui import BudgetCreateView, BudgetDetailView
 
 urlpatterns = [
     path('budgets/create', BudgetCreateView.as_view(), name='budget_create'),
+    path('budgets/<pk>/', BudgetDetailView.as_view(), name='budget_detail'),
 ]
--- apps/budgets/views/ui.py
+++ apps/budgets/views/ui.py
@@ -1,10 +1,11 @@
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
 
 from apps.budgets.forms import BudgetForm
 from apps.budgets.models import Budget
 
-class BudgetCreateView(CreateView):
+class BudgetCreateView(LoginRequiredMixin, CreateView):
     model = Budget
     form_class = BudgetForm
     template_name = 'budget_create.html'
@@ -13,3 +14,7 @@ class BudgetCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class BudgetDetailView(LoginRequiredMixin, DetailView):
+    model = Budget
+    template_name = 'budget_detail.html'

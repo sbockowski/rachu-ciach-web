e5ba7f126522896eb2f5545534ef8979a8287ca4
Szymon Boćkowski <sbockowskix@gmail.com>
2026-03-26 12:21:13 +0100

    Add initial budget detail implementation with categories and transactions (MVP)

--- apps/budgets/views/ui.py
+++ apps/budgets/views/ui.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.context_processors import request
 from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
 
 from apps.budgets.forms import BudgetForm
 from apps.budgets.models import Budget
+
 
 class BudgetCreateView(LoginRequiredMixin, CreateView):
     model = Budget
@@ -18,3 +20,13 @@ class BudgetCreateView(LoginRequiredMixin, CreateView):
 class BudgetDetailView(LoginRequiredMixin, DetailView):
     model = Budget
     template_name = 'budget_detail.html'
+
+    def get_queryset(self):
+        return Budget.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['budget_plans'] = self.object.plans.all()
+        context['transactions'] = self.object.transactions.all()
+
+        return context

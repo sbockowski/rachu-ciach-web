3bf5632c0bcbd5d68c61100a0e662acfdc8b8c5c
Szymon Boćkowski <sbockowskix@gmail.com>
2026-04-01 11:29:21 +0200

    Add transaction update

--- apps/transactions/urls/ui.py
+++ apps/transactions/urls/ui.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from apps.transactions.views.ui import TransactionCreateView, get_transaction_type
+from apps.transactions.views.ui import TransactionCreateView, get_transaction_type, TransactionUpdateView
 
 urlpatterns = [
     path('transactions/create/', TransactionCreateView.as_view(), name='transaction_create'),
     path('transactions/category-type/', get_transaction_type, name='transaction_category_type'),
-    # path('budgets/<pk>/update/', BudgetUpdateView.as_view(), name='budget_update'),
+    path('transactions/<pk>/update/', TransactionUpdateView.as_view(), name='transaction_update'),
+    # path('budgets/<pk>/delete/', BudgetDeleteView.as_view(), name='budget_delete'),
     # path('budgets/<pk>/delete/', BudgetDeleteView.as_view(), name='budget_delete'),
     # path('budgets/<pk>/', BudgetDetailView.as_view(), name='budget_detail'),
 ]
--- apps/transactions/views/ui.py
+++ apps/transactions/views/ui.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from apps.budgets.models import Category
 from apps.transactions.forms import TransactionForm
@@ -30,12 +30,31 @@ class TransactionCreateView(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
+class TransactionUpdateView(LoginRequiredMixin, UpdateView):
+    model = Transaction
+    form_class = TransactionForm
+    template_name = 'transaction_form.html'
+    success_url = reverse_lazy('dashboard')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
 def get_transaction_type(request):
     category_pk = request.GET.get('category')
     category = Category.objects.get(pk=category_pk)
 
     context = {
         'is_saving': category.type == 'saving',
-        'category_type': category.type
+        'category_type': category.type,
+        'current_type': request.GET.get('current_type', '')
     }
     return render(request, "transaction_type_field.html", context=context)
